fee0fb3aa90bee90f331ec8499403942a18578b5
liuran001 <bdovo@bdovo.cc>
2026-03-18 00:19:26 +0800

    Bump release version to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = "io.github.liuran001.fakebluetoothconnect"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.1"
     }
 
     buildTypes {
